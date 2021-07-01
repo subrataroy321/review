a5a96d63d0d4c82ae2b60bc755ae767a9f8730ce
SUBRATA ROY <67250069+subrataroy321@users.noreply.github.com>
2021-07-01 08:25:22 -0400

    create Array_JS_ES6.js and pased all the code from replit

---
+++ Array_JS_ES6.js
@@ -0,0 +1,78 @@
+var arrayNames = ['Brijesh', 'Subrata', 'Kajal', 'Shipra', 'Shaunak'];
+
+// console.log(arrayNames.toString());
+
+// console.log(arrayNames.join('_'));
+
+// arrayNames.push('Sajal');
+
+// arrayNames.pop();
+// var popedElement = arrayNames.pop();
+// console.log(popedElement);
+
+//arrayNames.unshift('Sajal');
+
+// arrayNames.shift();
+
+// delete arrayNames[0];
+
+// arrayNames.splice(1,0, "test");
+
+// arrayNames.splice(1,1, "test");
+
+//var newArray = arrayNames.slice(1,4); // slice method does not make changes to the actual array.
+
+// console.log(newArray);
+
+// arrayNames.sort(); // using Sort() you can only use it for Albhabets not for numbers
+
+// console.log(arrayNames);
+
+var arrayNos= [4, 1, 4, 7, 9, 67, 23, 92, 11, 26];
+
+// arrayNos.sort((a,b)=>{
+//   // return a-b; // for assending order
+//   return b-a; // for desending order
+// })
+
+// arrayNames.forEach((name, i)=>{
+//   console.log(name, '',i);
+// })
+
+// var mappedArray = arrayNos.map((val)=>{
+//   return val*2;
+// }) 
+// console.log(mappedArray);
+
+// var filteredArray = arrayNos.filter((val)=>{
+//   return val >10;
+// })
+
+// console.log(filteredArray);
+
+// console.log(Math.max(...arrayNos));
+// console.log(Math.min(...arrayNos)); 
+
+// There are more math fuctions can be used with numbers.
+
+
+// console.log(arrayNames)
+console.log(arrayNos);
+
+// var sum = arrayNos.reduce((total, val)=>{
+//   return total+val;
+// })
+// console.log(sum)
+
+// let test = arrayNos.every((val)=>{
+//   // return val>10; // false because all the elements are not greater than 10
+//   return val>0; // true because all the elements are greater than 0
+
+// })
+
+// let test = arrayNos.some((val)=>{
+//   // return val>100; // False Because there are no elements value is more than 100.
+//   return val>10; // True Because there are some elements value is more than 10.
+// })
+
+// console.log(test)
